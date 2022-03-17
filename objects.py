712deedec3c984b707a63d83930afdf0619d4962
KwaejenAepul <66973949+KwaejenAepul@users.noreply.github.com>
2022-03-17 16:11:14 +0100

    Introduced balll angles
    angle of the ball changes depending on where it hits the paddle (this does not fully work yet but whatever)

--- objects.py
+++ objects.py
@@ -3,32 +3,53 @@ from pygame.constants import K_DOWN, K_UP
 
 class pong_ball:
     def __init__ (self, x, y, size):
-        self.x = x
-        self.y = y
-        self.size = size
-        self.speed = 7
-        self.direction_x = self.speed
-        self.direction_y = self.speed
+        self.x:int = x
+        self.y:int = y
+        self.size:int = size
+        self.speed:int = 7
+        self.direction_x:int = self.speed
+        self.direction_y:int = self.speed
 
     def update(self, height, player, ai):
-        if self.x >= ai.x - self.size and self.y in range(int(ai.y), int(ai.y + ai.height)) and self.direction_x == self.speed:
-            self.direction_x = - self.speed
-        elif self.x <= player.x + player.width and self.y in range(int(player.y), int(player.y + player.height)) and self.direction_x == -self.speed:
+        if self.x >= ai.x - self.size and self.y in range(int(ai.y), int(ai.y + ai.height)) and self.direction_x > 0:
+            self.calc_angle(ai)
+            self.direction_x *= -1
+        elif self.x <= player.x + player.width and self.y in range(int(player.y), int(player.y + player.height)) and self.direction_x < 0:
+            self.calc_angle(player)
             self.direction_x = self.speed
-        if self.y >= height - self.size and self.direction_y == self.speed:
-            self.direction_y = - self.speed
-        elif self.y <= 0 and self.direction_y == -self.speed:
-            self.direction_y = self.speed
+        if self.y >= height - self.size and self.direction_y > 0:
+            self.direction_y *= -1
+        elif self.y <= 0 and self.direction_y < 0:
+            self.direction_y *= -1
         self.x += self.direction_x
         self.y += self.direction_y
 
+    def calc_angle(self, paddle):
+        section_size = int(paddle.height/5)
+        if self.x in range(paddle.x, int(paddle.x + section_size)):
+            self.direction_x = 4
+            self.direction_y = -10
+        elif self.x in range(paddle.x, int(paddle.x + (section_size*2))):
+            self.direction_x = 6
+            self.direction_y = -8
+        elif self.x in range(paddle.x, int(paddle.x + (section_size*3))):
+            self.direction_x = 7
+            self.direction_y = 7
+        elif self.x in range(paddle.x, int(paddle.x + (section_size*4))):
+            self.direction_x = 6
+            self.direction_y = 8
+        elif self.x in range(paddle.x, int(paddle.x + (section_size*5))):
+            self.direction_x = 4
+            self.direction_y = 10
+
+#paddle shit
 class paddle:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.width = 20
-        self.height = 150
-        self.speed = 5
+        self.x:int = x
+        self.y:int = y
+        self.width:int = 20
+        self.height:int = 150
+        self.speed:int = 5
 
 class Player(paddle):
     def __init__(self, x, y):
@@ -44,13 +65,12 @@ class Player(paddle):
             if self.y + self.speed > 0:
                 self.y -= self.speed
 
-
 class Ai(paddle):
     def __init__(self, x, y):
         paddle.__init__(self, x, y)
         self.score = 0
 
-
+    #unbeatable ai part
     def monado(self, bally, ballx, ballspeed, ballsize, direction, HEIGHT):
         i = 0
         direction = direction
@@ -73,7 +93,6 @@ class Ai(paddle):
         ballsize = int(ball.size)
         direction = int(ball.direction_y)
         bally = self.monado(bally, ballx, ballspeed, ballsize, direction, HEIGHT)
-        print(bally, ball.y)
         if bally > self.y:
             if self.y + self.height >= HEIGHT:
                 pass
